a438e237407e3b57e9f15abc300557a54a0c7b86
AcezTx <115911961+AcezTx@users.noreply.github.com>
2024-03-16 18:08:55 +0700

    GSLC_Assessment

--- app/src/main/java/com/example/gslc/MainActivity.java
+++ app/src/main/java/com/example/gslc/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.gslc;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Button btnShoes = findViewById(R.id.btnShoes);
+        btnShoes.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //ContainerView to ShoesFragment
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView, ShoesFragment.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name") // Name can be null
+                        .commit();
+            }
+        });
+
+        //Shoes to ShirtFragment
+        Button btnShirt = findViewById(R.id.btnShirt);
+        btnShirt.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView, ShirtFragment.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name") // Name can be null
+                        .commit();
+            }
+        });
+
+        Button btnFood = findViewById(R.id.btnFood);
+        btnFood.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView, FoodFragment.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name") // Name can be null
+                        .commit();
+            }
+        });
     }
 }
